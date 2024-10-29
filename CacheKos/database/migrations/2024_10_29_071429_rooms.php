558f4d12d07b2431f0d0446205e20d1ea15f042a
Shindi07 <shindicahyandari8@gmail.com>
2024-10-29 15:20:29 +0700

    migration

---
+++ CacheKos/database/migrations/2024_10_29_071429_rooms.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id(); 
+            $table->string('nama_kamar'); 
+            $table->string('nomor_kamar')->unique(); 
+            $table->float('harga_per_bulan'); 
+            $table->float('harga_per_hari');
+            $table->text('fasilitas'); 
+            $table->text('kelengkapan_lain'); 
+            $table->text('catatan_kamar'); 
+            $table->unsignedBigInteger('created_by'); 
+            $table->unsignedBigInteger('updated_by'); 
+            $table->timestamps(); 
+
+            // Menambahkan foreign key jika ada referensi ke tabel lain
+            // Misalnya jika created_by dan updated_by adalah id dari tabel users
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms'); // Menghapus tabel rooms jika migration dibatalkan
+    }
+};
