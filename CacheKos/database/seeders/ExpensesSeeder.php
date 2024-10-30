d7954b477b518bf8ee344ea1787fe0bbb70e4ca8
elaaaaa123 <elasumerti5@gmail.com>
2024-10-30 16:04:13 +0700

    nambah use db

--- CacheKos/database/seeders/ExpensesSeeder.php
+++ CacheKos/database/seeders/ExpensesSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
+use DB;
 
 
 class ExpensesSeeder extends Seeder
