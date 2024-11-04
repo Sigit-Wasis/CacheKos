787a47d1a4758957b486370a342b912ba7d71746
nida463 <nidaalhasani463@gmail.com>
2024-11-04 11:39:39 +0700

    add update user

--- CacheKos/routes/api.php
+++ CacheKos/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController; // Tambahkan ini
+// use App\Http\Controllers\API\ResidentController; // Tambahkan ini
 
 /*
 |--------------------------------------------------------------------------
@@ -18,10 +20,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('residents')->group(function () {
-    Route::get('/', [ResidentController::class, 'index']); // Get all residents
-    Route::post('/', [ResidentController::class, 'store']); // Create a new resident
-    Route::get('/{id}', [ResidentController::class, 'show']); // Get a single resident by ID
-    Route::put('/{id}', [ResidentController::class, 'update']); // Update a resident by ID
-    Route::delete('/{id}', [ResidentController::class, 'destroy']); // Delete a resident by ID
+
+// Routes untuk tabel 'users' (UserController)
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index']); // Get all users
+    Route::post('/', [UserController::class, 'create']); 
+    Route::get('/{id}', [UserController::class, 'edit']); // Get a single user by ID for editing
+    Route::put('/{id}', [UserController::class, 'update']); // Mengupdate data pengguna berdasarkan ID
 });
+ 
+
+
+// Route::prefix('residents')->group(function () {
+//     Route::get('/', [ResidentController::class, 'index']); // Get all residents
+//     Route::post('/', [ResidentController::class, 'store']); // Create a new resident
+//     Route::get('/{id}', [ResidentController::class, 'show']); // Get a single resident by ID
+//     Route::put('/{id}', [ResidentController::class, 'update']); // Update a resident by ID
+//     Route::delete('/{id}', [ResidentController::class, 'destroy']); // Delete a resident by ID
+// });
