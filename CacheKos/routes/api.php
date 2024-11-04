667d67377ace1edeade49d569da47769fe018c56
lillysky07 <deliaputriadel096@gmail.com>
2024-11-04 10:36:21 +0700

    setting create

--- CacheKos/routes/api.php
+++ CacheKos/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\SettingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,10 +19,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('residents')->group(function () {
-    Route::get('/', [ResidentController::class, 'index']); // Get all residents
-    Route::post('/', [ResidentController::class, 'store']); // Create a new resident
-    Route::get('/{id}', [ResidentController::class, 'show']); // Get a single resident by ID
-    Route::put('/{id}', [ResidentController::class, 'update']); // Update a resident by ID
-    Route::delete('/{id}', [ResidentController::class, 'destroy']); // Delete a resident by ID
+// Route::prefix('residents')->group(function () {
+//     Route::get('/', [ResidentController::class, 'index']); // Get all residents
+//     Route::post('/', [ResidentController::class, 'store']); // Create a new resident
+//     Route::get('/{id}', [ResidentController::class, 'show']); // Get a single resident by ID
+//     Route::put('/{id}', [ResidentController::class, 'update']); // Update a resident by ID
+//     Route::delete('/{id}', [ResidentController::class, 'destroy']); // Delete a resident by ID
+// });
+
+Route::prefix('settings')->group(function () {
+    Route::get('/', [SettingController::class, 'index']);
+    Route::post('/', [SettingController::class, 'store']);
+    Route::get('/{id}', [SettingController::class, 'show']);
+    Route::put('/{id}', [SettingController::class, 'update']);
+    Route::delete('/{id}', [SettingController::class, 'destroy']);
 });
