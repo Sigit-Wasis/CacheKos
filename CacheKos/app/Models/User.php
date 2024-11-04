e46de61b2342d5dba3806dcfcdd4b6e0a0511442
elaaaaa123 <elasumerti5@gmail.com>
2024-11-04 16:49:58 +0700

    logout

--- CacheKos/app/Models/User.php
+++ CacheKos/app/Models/User.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens; // Menambahkan use statement untuk HasApiTokens
