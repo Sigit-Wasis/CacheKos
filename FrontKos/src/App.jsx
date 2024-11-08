cddcb548656ec98a60695588fd48540d1af0daea
Kishi-Nr <satrianurramadhan@gmail.com>
2024-11-08 11:15:46 +0700

    residen

--- FrontKos/src/App.jsx
+++ FrontKos/src/App.jsx
@@ -1,26 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, Link , useNavigate} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import Room from "./pages/Room";
-<<<<<<< HEAD
 import Resident from "./pages/Resident";
 import AddResidentPage from "./pages/AddResidentPage";
-=======
-import Setting from "./pages/Setting";
-import Payment from "./pages/Payment";
->>>>>>> bd5744d872b63cb7c250fca1fe82defc5a7c545a
+import InactiveResidentsPage from "./pages/InactiveResidentsPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
       setIsAuthenticated(true);
-      // navigate("/home");
     }
   }, [navigate]);
 
@@ -67,23 +64,33 @@ function App() {
                       Room
                     </Link>
                   </li>
-                  <li className="nav-item">
-<<<<<<< HEAD
-                    <Link className="nav-link" to="/resident">
+
+                  {/* Resident Dropdown */}
+                  <li className="nav-item dropdown">
+                    <Link
+                      className="nav-link dropdown-toggle"
+                      to="#"
+                      id="residentDropdown"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
                       Resident
                     </Link>
+                    <ul className="dropdown-menu" aria-labelledby="residentDropdown">
+                      <li>
+                        <Link className="dropdown-item" to="/resident">
+                          Resident
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item" to="/inactiveresident">
+                          Inactive Resident
+                        </Link>
+                      </li>
+                    </ul>
                   </li>
-=======
-                <Link className="nav-link" to="/setting">
-                  Setting 
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/payment">
-                  Payment
-                </Link>
-              </li>
->>>>>>> bd5744d872b63cb7c250fca1fe82defc5a7c545a
+
                   <li className="nav-item">
                     <Link
                       className="nav-link btn btn-link"
@@ -98,19 +105,15 @@ function App() {
             </ul>
           </div>
         </div>
-      </nav> 
+      </nav>
       <Routes>
         <Route path="/login" element={<LoginPage setIsAuthenticated={setIsAuthenticated} />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
-<<<<<<< HEAD
-=======
-        <Route path="/setting" element={<Setting />} />
-        <Route path="/payment" element={<Payment />} />
->>>>>>> bd5744d872b63cb7c250fca1fe82defc5a7c545a
         {isAuthenticated && <Route path="/room" element={<Room />} />}
-        {/* {isAuthenticated && <Route path="/resident" element={<Resident/>} />} */}
-        {/* {isAuthenticated && <Route path="/add" element={<AddResidentPage/>} />} */}
+        {isAuthenticated && <Route path="/resident" element={<Resident />} />}
+        {isAuthenticated && <Route path="/add" element={<AddResidentPage />} />}
+        {isAuthenticated && <Route path="/inactiveresident" element={<InactiveResidentsPage />} />}
       </Routes>
     </div>
   );
