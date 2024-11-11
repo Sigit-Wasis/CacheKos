a093e14ca140d58067596c3627be6025733ad16b
Shindi07 <shindicahyandari8@gmail.com>
2024-11-11 16:10:36 +0700

    room

--- CacheKos/app/Models/Room.php
+++ CacheKos/app/Models/Room.php
@@ -11,8 +11,10 @@ class Room extends Model
     protected $fillable = [
         'nama_kamar',
         'nomor_kamar',
-        'harga_per_bulan',
         'harga_per_hari',
+        'harga_per_minggu',
+        'harga_per_bulan',
+        'harga_per_tahun',
         'fasilitas',
         'kelengkapan_lain',
         'catatan_kamar',
